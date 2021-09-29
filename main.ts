2b3d2cca425ebe415b67df9199b9763378be73c7
Jim Egenrieder <jimegenrieder@gmail.com>
2021-09-28 21:27:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,33 +4,33 @@ input.onGesture(Gesture.Shake, function () {
 let timer = 0
 basic.showLeds(`
     . # # # .
-    . # # # .
-    . # # # .
+    # . # . #
+    # # # # #
     . # # # .
     . # # # .
     `)
 basic.forever(function () {
     basic.pause(1000)
     timer += 1
-    if (timer == 30) {
+    if (timer == 15) {
         basic.showLeds(`
             . . . . .
             . # # # .
             . # # # .
             . . . . .
-            . . . . .
-            `)
-    }
-    if (timer == 20) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
             . . . . .
             `)
     }
     if (timer == 10) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (timer == 5) {
         basic.showLeds(`
             . # # # .
             . # # # .
